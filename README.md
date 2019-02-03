e7dbdd21e593577e164d27d8b6f02e4cdcbff138
Xie yuhui <xyuh111@qq.com>
2019-02-03 23:06:54 +0800

    首次构建项目

---
+++ README.md
@@ -0,0 +1,18 @@
+# JDFinance
+这是仿京东金融的一个模块化单页面的 vue 项目。
+
+```
+git clone https://github.com/xyuh111/JDFinance.git
+```
+
+```
+cd JDFinance
+```
+
+```
+npm i
+```
+
+```
+npm start
+```
---
+++ app/js/home/index.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+    <div :class="$style.home">
+        <h1>Home</h1>
+        <p>{{ name }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: "xieyuhui",
+        }
+    },
+}
+</script>
+
+<style lang="scss" module>
+@import '../../css/reset.scss';
+.home {
+    color: red;
+    font-size: 60px;
+}
+</style>
